e8d491aa68d1271b68a683a7413fa125ad35f194
chris <chris@syn.sh>
2025-04-19 16:06:33 -0700

    moved direnv to homemanager and added layout_uv

--- users/framework/home-manager.nix
+++ users/framework/home-manager.nix
@@ -91,6 +91,12 @@ in
   #
   programs.bat.enable = true;
 
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true; # see note on other shells below
+    nix-direnv.enable = true;
+  };
+
   programs.eza = {
     enable = true;
     enableZshIntegration = true;
@@ -400,6 +406,23 @@ in
   home.file = {
     ".ssh/sockets/.keep".text = "# Managed by Home Manager";
     ".zsh_functions".source = ../../programs/zsh/functions;
+
+    ".direnvrc".text = ''
+      layout_uv() {
+        watch_file .python-version pyproject.toml uv.lock
+        uv sync --frozen || true
+        venv_path="$(expand_path "''${UV_PROJECT_ENVIRONMENT:-.venv}")"
+        if [[ -e $venv_path ]]; then
+            VIRTUAL_ENV="$(pwd)/.venv"
+            PATH_add "$VIRTUAL_ENV/bin"
+            export UV_ACTIVE=1  # or VENV_ACTIVE=1
+            export VIRTUAL_ENV
+        fi
+        if [[ ! -e $venv_path ]]; then
+            log_status "No virtual environment exists. Executing \`uv venv\` to create one."
+        fi
+      }
+    '';
 
     # TODO: mkif isLinux
     ".config/pop-shell/config.json".text = ''
