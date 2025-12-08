fc706b6e67ab176b8765fa63f79ff5587a16fe91
chris <chris@syn.sh>
2025-12-08 12:27:08 -0800

    darwin - removed packages managed externally

--- nix/common/hm-common-pkgs.nix
+++ nix/common/hm-common-pkgs.nix
@@ -4,16 +4,12 @@
 
 [
   # CLI Tools
-  pkgs.awscli2
   pkgs.bottom
   pkgs.dig
-  pkgs.docker-compose
   pkgs.file
   pkgs.htop
   pkgs.jq
-  pkgs.kubectl
   pkgs.kube-prompt
-  pkgs.kubernetes-helm
   pkgs.lsof
   pkgs.minikube
   pkgs.nh # Yet another Nix CLI helper - https://github.com/nix-community/nh
