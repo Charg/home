1c9912331319f3de0e519929e35b18779dc7bd56
chris <chris@syn.sh>
2025-12-27 20:19:29 -0800

    chore: nix - added more common tools

--- nix/common/hm-common-pkgs.nix
+++ nix/common/hm-common-pkgs.nix
@@ -5,8 +5,12 @@
 [
   # CLI Tools
   pkgs.bottom
+  pkgs.crane # Tools for interacting with remote images and registries including crane and gcrane
   pkgs.dig
+  pkgs.dive # Tool for exploring each layer in a docker image
+  pkgs.dockle # Container Image Linter for Security
   pkgs.file
+  pkgs.grype # Vulnerability scanner for container images and filesystems
   pkgs.htop
   pkgs.jq
   pkgs.kube-prompt
@@ -20,8 +24,10 @@
   pkgs.ripgrep
   pkgs.sqlite
   pkgs.tmux
+  pkgs.trivy # Simple and comprehensive vulnerability scanner for containers, suitable for CI
   pkgs.unzip
   pkgs.uv
+  pkgs.whois
   pkgs.yubikey-manager
   pkgs.zoxide
 
