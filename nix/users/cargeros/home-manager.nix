99021225ca3bc4de5ff923e61ef169074a0f1399
chris <chris@syn.sh>
2026-05-07 18:02:33 -0700

    feat(nix): added claude code to work

--- nix/users/cargeros/home-manager.nix
+++ nix/users/cargeros/home-manager.nix
@@ -17,6 +17,7 @@ in
 {
   imports = [
     ../../common/hm-common.nix
+    ../../programs/claude-code/hm.nix
     ../../programs/delta/hm.nix
     ../../programs/direnv/hm.nix
     ../../programs/eza/hm.nix
