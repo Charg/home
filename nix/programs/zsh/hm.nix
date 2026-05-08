6e075f8c76f3e7814025fc4ee3096f541ce3b99e
chris <chris@syn.sh>
2026-05-07 18:54:46 -0700

    feat(nix): added gcmm alias

--- nix/programs/zsh/hm.nix
+++ nix/programs/zsh/hm.nix
@@ -143,15 +143,17 @@
       ga = "git add";
       gap = "git add --patch";
       gbls = "git branch -l --sort=-committerdate --format='%(authordate:short) %(color:yellow)%(refname:short) %(color:blue)%(subject)%(color:reset)  (%(color:green)%(committerdate:relative)%(color:reset))'";
-      gcb = "git checkout -b";
-      gch = "git ch";
-      gcm = "git commit --message";
-      gco = "git checkout";
-      gfm = "git pull";
+      gca  = "git commit --ammend";
+      gcb  = "git checkout -b";
+      gch  = "git ch";
+      gcm  = "git commit";
+      gcmm = "git commit --message";
+      gco  = "git checkout";
+      gfm  = "git pull";
       git_show_tag_by_date = "git log --tags --simplify-by-decoration --pretty='format:%ai %d'";
-      gp = "git push";
+      gp  = "git push";
       gpb = "git pushb";
-      gs = "git stash";
+      gs  = "git stash";
       gtr = "cd $(git rev-parse --show-cdup)";
 
       #
